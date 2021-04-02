8e432d2a45f6861ffeb0c7894045c2bb58d046d0
Luciana001 <luciana.brancato@gmail.com>
2021-04-02 16:43:00 +0200

    fix conflicts 2

--- src/product.php
+++ src/product.php
@@ -9,11 +9,7 @@ try {
     exit;
 }
 
-<<<<<<< HEAD
-if(isset($_GET['code'])) {
-=======
 if (isset($_GET['code'])) {
->>>>>>> 59763901ab52503a93c31a7ae729e60633ea6c5f
     $code = $_GET['code'];
 
     try {
@@ -29,22 +25,14 @@ if (isset($_GET['code'])) {
         $results->bindParam(1, $code, PDO::PARAM_STR);
         //To execute the query set into results object
         $results->execute();
-<<<<<<< HEAD
-    } catch(Exception $e) {
-=======
     } catch (Exception $e) {
->>>>>>> 59763901ab52503a93c31a7ae729e60633ea6c5f
         echo $e->getMessage();
         exit;
     }
     // To retreive the information for the one product that matches the ID
     $product = $results->fetch(PDO::FETCH_ASSOC);
 
-<<<<<<< HEAD
-    if($product == FALSE) {
-=======
     if ($product == FALSE) {
->>>>>>> 59763901ab52503a93c31a7ae729e60633ea6c5f
         echo "This code reference $code doesn't exist in the Database. </br> <a href='/''>Go back</a>";
         die();
     }
