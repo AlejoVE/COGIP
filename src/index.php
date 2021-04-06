597278d7b9fa49ae0e43eccf785f95bc15dc7808
Josue-U <josueulliana@gmail.com>
2021-04-06 09:46:08 +0200

    suppression requete sql en com

--- src/index.php
+++ src/index.php
@@ -29,41 +29,6 @@ $companies = $resultsCompanies->fetchAll();
 
 $name='';
 
-// $results = $db->query("SELECT * FROM invoices JOIN companies ON company_id = id_comp ORDER BY id_comp DESC LIMIT 0,5 ");
-// echo '<strong><hr>' . 'Last invoices: '  . '<br>';
-// echo '<br>';
-// while ($donnees = $results->fetch()) {
-//     $date = $donnees['invoice_date'];
-//     $strY = substr($date, 0, 4);
-//     $strM = substr($date, 5, -3);
-//     $strD = substr($date, 8, 9);
-
-//     echo "F" . $strY . $strM . $strD . "-" . $donnees['invoice_id'] . " | " . $strD . "/" . $strM . "/" . $strY .
-//         " | " . $donnees['name'] . ' | ' . '<br>';
-// }
-// $results->closeCursor();
-
-// echo '<br>';
-
-// $results = $db->query("SELECT * FROM people JOIN companies ON company_id = id_comp ORDER BY id_comp DESC  LIMIT 0,5 ");
-// echo '<strong><hr>' . 'Last contacts : '  . '<br>';
-// echo '<br>';
-// while ($donnees = $results->fetch()) {
-//     echo $donnees['first_name'] . " " . $donnees['last_name'] . " | " . $donnees['phone'] . " | " . $donnees['email'] . " | " . $donnees['name'] . '<br>';
-// }
-// $results->closeCursor();
-
-// echo '<br>';
-
-// $results = $db->query("SELECT * FROM companies JOIN type_of_company ON id_type = typeId ORDER BY id_comp DESC LIMIT 0,5 ");
-// echo '<strong><hr>' . 'Last companies : '  . '<br>';
-// echo '<br>';
-// while ($donnees = $results->fetch()) {
-//     echo $donnees['name'] . " | " . $donnees['number_vta'] . " | " . $donnees['country'] . " | " . $donnees['type'] . '<br>';
-// }
-// $results->closeCursor();
-
-
 
 ?>
 <!DOCTYPE html>
