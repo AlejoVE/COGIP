4d1d9729725401bf01d3d6cbcd27572bb16b9dd0
Josue-U <josueulliana@gmail.com>
2021-04-07 12:12:40 +0200

    login.php in progress

--- src/login.php
+++ src/login.php
@@ -1,4 +1,27 @@
 <?php
+session_start();
+
+try {
+    $db = new PDO("mysql:host=remotemysql.com;dbname=nJpHWU5rJ5;port=3306", "nJpHWU5rJ5", "VnjcIEPzgV");
+    //$db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $error) {
+    echo $error->getMessage();
+    exit;
+}
+
+
+if(isset($_POST['login'])){
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+}
+//------------------when the correct code is done--------------------------------------------------------------
+// <?php
+// if(isset($_SESSION['username'])){
+//     echo "<center><h1>Bonjour ".$_SESSION['username']. " Comment allez vous aujourd'hui ? :)</h1></center>";
+// }
+//  à mettre dans le <body> de la page index Admin/moderator.php ET ne pas oublier le sessont_start
 
 ?>
 
@@ -17,10 +40,11 @@
 </head>
 
 <body>
+  
 <div class="formLogin">
     <form method="POST" action="login.php">
-        <input type="text" name="username" value="username" ></input>
-        <input type="password" name="password"></input>
+        <input type="text" name="username" value="username" >: username</input>
+        <input type="password" name="password">: password</input>
         <input type="submit" name="login" value="login"></input>
     </form>
     <a href="index.php">BACK</a>
