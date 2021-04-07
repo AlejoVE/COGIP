4770ae34fe766affee79315ea52d888a71e3d22a
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-04-07 15:15:13 +0200

    Add invoices to invoice.php

--- src/Model/InvoiceManager.php
+++ src/Model/InvoiceManager.php
@@ -2,3 +2,15 @@
 
 
 declare(strict_types=1);
+require_once 'Manager.php';
+
+class InvoiceManager extends Manager 
+{
+    public function getAllInvoices () 
+    {
+            $db = $this->connectDb();
+            $invoices = $db->query("SELECT * FROM invoices JOIN companies ON company_id = id_comp JOIN type_of_company ON id_type = typeId ");
+            $result = $invoices->fetchAll(PDO::FETCH_ASSOC);
+            return $result;
+    }
+}
