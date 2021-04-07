82b09d6e4fd2491cabe798cb53d24570925eba27
Alejandro Montilla <luis.alejandro.499be@gmail.com>
2021-04-07 15:32:28 +0200

    Get companies suppliers and clients

--- src/Model/CompaniesManager.php
+++ src/Model/CompaniesManager.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-namespace Becode\MVCBoilerplate\model;
+// namespace Becode\MVCBoilerplate\model;
 
 require_once('Manager.php');
 
@@ -11,24 +11,41 @@ class CompaniesManager extends Manager
 
     private $name;
 
-    public function __construct(string $name)
-    {
-        $this->name = $name;
-    }
+    // public function __construct(string $name)
+    // {
+    //     $this->name = $name;
+    // }
 
-    //To display all the companies
-    public function getCompanies()
+    public function getCompanies($type) 
     {
-        
         $db = $this->connectDb();
 
-        $req = $db->query('SELECT ... AS ... 
-            FROM ... 
-            ORDER BY ... 
-            DESC');
-
-        return $req->fetch(PDO::FETCH_ASSOC);
+        $suppliers = $db->query("SELECT * FROM companies JOIN type_of_company ON id_type = typeId WHERE id_type=$type");
+        $result = $suppliers->fetchAll(PDO::FETCH_ASSOC);
+        return $result;
     }
+
+    // public function getClients() {
+    //     $db = $this->connectDb();
+
+    //     $suppliers = $db->query("SELECT * FROM companies JOIN type_of_company ON id_type = typeId WHERE id_type=2 ");
+    //     $result = $suppliers->fetchAll(PDO::FETCH_ASSOC);
+    //     return $result;
+    // }
+
+    //To display all the companies
+    // public function getCompanies()
+    // {
+        
+    //     $db = $this->connectDb();
+
+    //     $req = $db->query('SELECT ... AS ... 
+    //         FROM ... 
+    //         ORDER BY ... 
+    //         DESC');
+
+    //     return $req->fetch(PDO::FETCH_ASSOC);
+    // }
 
     //To display company by id
     public function getCompany(int $companyId)
