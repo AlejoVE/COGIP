0b632d86a241db52aa4caea92119982c39f83d3f
Josue-U <josueulliana@gmail.com>
2021-04-08 16:06:02 +0200

    add new update 1

--- src/index.php
+++ src/index.php
@@ -46,7 +46,9 @@ $name='';
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <link rel="stylesheet" type="text/css" href="">
+        <link rel="stylesheet" type="text/css" href="styles/normalize.css">
+        <link rel="stylesheet" type="text/css" href="styles/style.css">
+        <link rel="icon" type="image/png"  href="styles/img/cogip-logo.jpeg">
         <title>Home</title>
     </head>
     <body>
--- src/login.php
+++ src/login.php
@@ -1,31 +1,4 @@
-<?php
-session_start();
-
-try {
-    $db = new PDO("mysql:host=remotemysql.com;dbname=nJpHWU5rJ5;port=3306", "nJpHWU5rJ5", "VnjcIEPzgV");
-    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-} catch (Exception $error) {
-    echo $error->getMessage();
-    exit;
-}
-
-$username = htmlspecialchars($_POST['username']);
-$password = htmlspecialchars($_POST['password']);
-
-//$password = password_hash($password); to secure the password
-
-if(isset($_POST['login'])){
-
-    
-
-}
-//------------------when the correct code is done--------------------------------------------------------------
-// <?php
-// if(isset($_SESSION['username'])){
-//     echo "<center><h1>Bonjour ".$_SESSION['username']. " Comment allez vous aujourd'hui ? :)</h1></center>";
-// }
-//  à mettre dans le <body> de la page index Admin/moderator.php ET ne pas oublier le sessont_start
-
+<?php  //if(isset) connexion 
 ?>
 
 <!DOCTYPE html>
@@ -43,19 +16,12 @@ if(isset($_POST['login'])){
 </head>
 
 <body>
-  
-<div class="formLogin">
-    <form method="POST" action="login.php">
-
-        <div class="label">Username</div>
-        <input type="text" name="username" value="username" ></input>
-
-        <div class="label">Password</div>
-        <input type="password" name="password"></input>
-
-        <input type="submit" name="login" value="login"></input>
+<div class="formConnexion">
+    <form>
+        <input type="text" name="identification" ></input>
+        <input type="text" name="password" value="password"></input>
     </form>
-    <a href="index.php">BACK</a>
+    <a href="index.php">GO BACK</a>
 </div>
 
 </body>
