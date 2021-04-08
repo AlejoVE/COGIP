f1ab95e1584fe8b72662a9bc0ae18f15a03a1b0b
Josue-U <josueulliana@gmail.com>
2021-04-08 11:44:52 +0200

    add login.php

--- src/login.php
+++ src/login.php
@@ -1,4 +1,31 @@
-<?php  //if(isset) connexion 
+<?php
+session_start();
+
+try {
+    $db = new PDO("mysql:host=remotemysql.com;dbname=nJpHWU5rJ5;port=3306", "nJpHWU5rJ5", "VnjcIEPzgV");
+    $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (Exception $error) {
+    echo $error->getMessage();
+    exit;
+}
+
+$username = htmlspecialchars($_POST['username']);
+$password = htmlspecialchars($_POST['password']);
+
+//$password = password_hash($password); to secure the password
+
+if(isset($_POST['login'])){
+
+    
+
+}
+//------------------when the correct code is done--------------------------------------------------------------
+// <?php
+// if(isset($_SESSION['username'])){
+//     echo "<center><h1>Bonjour ".$_SESSION['username']. " Comment allez vous aujourd'hui ? :)</h1></center>";
+// }
+//  à mettre dans le <body> de la page index Admin/moderator.php ET ne pas oublier le sessont_start
+
 ?>
 
 <!DOCTYPE html>
@@ -16,12 +43,19 @@
 </head>
 
 <body>
-<div class="formConnexion">
-    <form>
-        <input type="text" name="identification" ></input>
-        <input type="text" name="password" value="password"></input>
+  
+<div class="formLogin">
+    <form method="POST" action="login.php">
+
+        <div class="label">Username</div>
+        <input type="text" name="username" value="username" ></input>
+
+        <div class="label">Password</div>
+        <input type="password" name="password">: password</input>
+
+        <input type="submit" name="login" value="login"></input>
     </form>
-    <a href="index.php">GO BACK</a>
+    <a href="index.php">BACK</a>
 </div>
 
 </body>
